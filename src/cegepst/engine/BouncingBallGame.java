837799613021934d63020f860a85c57b98cc9a48
Damien de la Bruère-Terreault <damiendlbt@gmail.com>
2020-10-09 16:36:24 -0400

    Ajout de la notion de fps au lieu d'un sleep dans la boucle d'update.

--- src/cegepst/engine/BouncingBallGame.java
+++ src/cegepst/engine/BouncingBallGame.java
@@ -6,11 +6,15 @@ public class BouncingBallGame extends Game{
 
     private Ball ball;
     private Ball ball2;
+    private Ball ball3;
+    private Ball ball4;
     private int score = 0;
 
     public BouncingBallGame() {
         ball = new Ball(25);
         ball2 = new Ball(50);
+        ball3 = new Ball(10);
+        ball4 = new Ball(40);
     }
 
     @Override
@@ -27,7 +31,9 @@ public class BouncingBallGame extends Game{
     public void update() {
         ball.update();
         ball2.update();
-        if (ball.hasTouchBound() || ball2.hasTouchBound()) {
+        ball3.update();
+        ball4.update();
+        if (ball.hasTouchBound() || ball2.hasTouchBound() || ball3.hasTouchBound() || ball4.hasTouchBound()) {
             score += 10;
         }
     }
@@ -36,6 +42,10 @@ public class BouncingBallGame extends Game{
     public void draw(Buffer buffer) {
         ball.draw(buffer, Color.RED);
         ball2.draw(buffer, Color.GREEN);
+        ball3.draw(buffer, Color.blue);
+        ball4.draw(buffer, Color.MAGENTA);
         buffer.drawText(10, 20, "Score: " + score, Color.white);
+        buffer.drawText(10,40, "FPS: " + GameTime.getCurrentFps(), Color.white);
+        buffer.drawText(10,60,"Time: " + GameTime.getElapsedFormattedTime(), Color.white);
     }
 }
